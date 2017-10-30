2a71a1e80201213536859e95fdda1b821822cb9b
Krzysiu <kheciek@gmail.com>
2017-10-30 13:48:21 +0100

    update

--- script.js
+++ script.js
@@ -4,20 +4,15 @@ console.log(hello + " " + world);
 const grades = [1, 5, 5, 5, 4, 3, 3, 2, 1];
 const table = [1, 4, 'Iwona', false, 'Nowak'];
 
-const multiply = (a = 1, b = 1) => console.log(`Multiply: ` + a * b);
+const multiply = (a, b = 1) => {return (a * b)};
 const average = (...args) => {
-    let value = 0;
-    let amount = 0;
-    args.forEach(arg => {
-        value += arg;
-        amount += 1;
-    });
-    console.log(`Average: ` + value/amount);
+    const value = args.reduce((total, argument) => total += argument);
+    return (value / args.length);
 };
 const [ , , firstName, , lastName] = table;
 
-multiply(5, 6);
-multiply(9);
-average(1, 2, 3);
-average(...grades);
+console.log(`Mutltiply(5, 6) = ` + multiply(5, 6));
+console.log(`Mutltiply(9) = ` + multiply(9));
+console.log(`Average(1, 2, 3) = ` + average(1, 2, 3));
+console.log(`Average(...grades) = ` + average(...grades));
 console.log(`Name: ` + firstName + ` ` + lastName);
